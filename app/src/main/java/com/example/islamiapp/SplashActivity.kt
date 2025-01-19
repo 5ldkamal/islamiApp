c9ddd9440f794e5508b5a34816fce6807c61eba0
KhaledKamal <khaled.kamal@samesystem.com>
2025-01-19 14:58:27 +0200

    Create menu and its items

--- app/src/main/java/com/example/islamiapp/SplashActivity.kt
+++ app/src/main/java/com/example/islamiapp/SplashActivity.kt
@@ -1,6 +1,9 @@
 package com.example.islamiapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,5 +13,11 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        Handler(Looper.getMainLooper()).postDelayed({
+            // Open the new activity
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish() // Optional: Finish current activity
+        },2000)
     }
 }
